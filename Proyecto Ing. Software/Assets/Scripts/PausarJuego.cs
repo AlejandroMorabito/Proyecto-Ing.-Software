67fb2628e6d36dc28900c920aed6043d6cb20c7c
centromundox <71676092+VirginiaSTR@users.noreply.github.com>
2025-05-30 13:58:06 -0400

    Menú de Pausa. Iniciando

---
+++ Proyecto Ing. Software/Assets/Scripts/PausarJuego.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+
+public class PausarJuego : MonoBehaviour
+{
+    public GameObject Inicio;
+    public bool juegoPausado = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+    }
+        
+        if (juegoPausado)
+        {
+            Reanudar();
+
+        }
+        else
+        {
+            Pausar();
+        }
+    }
+
+    public void Reanudar()
+        {
+            menuPausa.SetActive(false);
+            Time.timeScale = 1;
+            juegoPausado = false;
+        }
+    public void Pausar()
+        {
+            menuPausa.SetActive(true);
+            Time.timeScale = 0;
+            juegoPausado = true;
+        }
+}
