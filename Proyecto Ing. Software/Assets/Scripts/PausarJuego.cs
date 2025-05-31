467a88502f900629f41796e6f2ad60402ca7206b
AlejandroMorabito <131191251+AlejandroMorabito@users.noreply.github.com>
2025-05-30 23:51:09 -0400

    errores corregidos
    errores de menu pausa corregidos

--- Proyecto Ing. Software/Assets/Scripts/PausarJuego.cs
+++ Proyecto Ing. Software/Assets/Scripts/PausarJuego.cs
@@ -1,40 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class PausarJuego : MonoBehaviour
 {
-    public GameObject Inicio;
+    public GameObject menuPausa; // Cambiado de "Inicio" a "menuPausa" para coincidir con tu código
     public bool juegoPausado = false;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-        }
-        
+        {
             if (juegoPausado)
             {
                 Reanudar();
-
             }
             else
             {
                 Pausar();
             }
+        }
     }
 
-public void Reanudar()
-        {
-            menuPausa.SetActive(false);
-            Time.timeScale = 1;
-            juegoPausado = false;
-        }
-public void Pausar()
-        {
-            menuPausa.SetActive(true);
-            Time.timeScale = 0;
-            juegoPausado = true;
-        }
+    public void Reanudar()
+    {
+        menuPausa.SetActive(false);
+        Time.timeScale = 1f;
+        juegoPausado = false;
+    }
+
+    public void Pausar()
+    {
+        menuPausa.SetActive(true);
+        Time.timeScale = 0f;
+        juegoPausado = true;
+    }
 }
